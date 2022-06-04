645f2ce19dcf4848c94ded1e94bd2d35d170b167
Patrick Curran <patrickcurran48@hotmail.com>
2022-06-05 00:35:57 +0100

    backend herok with bctyptjs

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import { db } from "../models/db.js";
 
 const saltRounds = 10;
@@ -20,7 +20,7 @@ export const accountsController = {
     auth: false,
     handler: async function (request, h) {
       const user = request.payload;
-      user.password = await bcrypt.hash(user.password, saltRounds);
+      user.password = await bcryptjs.hash(user.password, saltRounds);
       await db.userStore.addUser(user);
       return h.redirect("/");
     },
@@ -36,7 +36,7 @@ export const accountsController = {
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
-      const passwordsMatch = await bcrypt.compare(password, user.password);
+      const passwordsMatch = await bcryptjs.compare(password, user.password);
       if (!user || !passwordsMatch) {
         return h.redirect("/");
       }
